564fe5f07466dc8c494f70e8dbd5b6058459397f
patrik <patrik.youssef@arbetsformedlingen.se>
2023-11-30 22:48:11 +0100

    Updates copy

--- app/components/elevatorInfo/ElevatorInfo.tsx
+++ app/components/elevatorInfo/ElevatorInfo.tsx
@@ -8,29 +8,26 @@ import Error from '../error/Error';
 const ElevatorInfo: React.FC<ElevatorInfoProps> = ({ elevator }) => {
   const { floorCount, elevatorDirection, isAtDestination, error } = useUpdateElevators(elevator);
 
+  const altText = elevatorDirection === DirectionEnum.UP ? 'Hissen åker upp' : 'Hissen åker ner';
+
   return (
     <div className={styles['elevator-info-container']} role="region" aria-live="assertive">
-    {!isAtDestination && (
-      <>
-        <span className={styles['elevator-direction']} aria-hidden="true">
-          {elevatorDirection === DirectionEnum.UP ? (
-            <Image src={'/north.svg'} alt="upp" width={180} height={37} priority />
-          ) : (
-            <Image src={'/south.svg'} alt="upp" width={180} height={37} priority />
-          )}
-        </span>
-        <span className={styles['elevator-floor']}>
-          {floorCount} destination {elevator.destination}
-        </span>
-      </>
-    )}
-    {isAtDestination && (
-      <p>Framme, i våning {elevator.destination}</p>
-    )}
+      {!isAtDestination && (
+        <>
+          <span className={styles['elevator-direction']}>
+            <Image src={elevatorDirection === DirectionEnum.UP ? '/north.svg' : '/south.svg'} alt={altText} width={180} height={37} priority />
+          </span>
+          <span className={styles['elevator-floor']}>
+            {floorCount} destination {elevator.destination}
+          </span>
+        </>
+      )}
+      {isAtDestination && (
+        <p>Framme, i våning {elevator.destination}</p>
+      )}
 
-  {error && <Error errorId="technicalError" errorMsg={ `Tyvärr inträffade ett tekniskt fel "${error}"`} />}
-
-  </div>
+      {error && <Error errorId="technicalError" errorMsg={`Tyvärr inträffade ett tekniskt fel "${error}"`} />}
+    </div>
   );
 };
 
