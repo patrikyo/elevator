67d3128fb540a342bf25a409ce86f303471320d1
patrik <patrik.youssef@arbetsformedlingen.se>
2023-12-01 09:29:34 +0100

    Fixes styling and adds new image

--- app/components/elevatorInfo/ElevatorInfo.tsx
+++ app/components/elevatorInfo/ElevatorInfo.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import ElevatorInfoProps from '../../interfaces/ElevatorInfoProps.interface';
 import { DirectionEnum } from '../../constants/direction.enum';
 import Image from 'next/image';
-import styles from './ElevatorInfo.module.css'; 
+import styles from './ElevatorInfo.module.css';
 import useUpdateElevators from '../../hooks/useUpdateElevators';
 import Error from '../error/Error';
 
@@ -15,7 +16,7 @@ const ElevatorInfo: React.FC<ElevatorInfoProps> = ({ elevator }) => {
       {!isAtDestination && (
         <>
           <span className={styles['elevator-direction']}>
-            <Image src={elevatorDirection === DirectionEnum.UP ? '/north.svg' : '/south.svg'} alt={altText} width={180} height={37} priority />
+            <Image src={elevatorDirection === DirectionEnum.UP ? '/north.svg' : '/south.svg'} alt={altText} width={200} height={50} priority />
           </span>
           <span className={styles['elevator-floor']}>
             {floorCount} destination {elevator.destination}
@@ -23,9 +24,15 @@ const ElevatorInfo: React.FC<ElevatorInfoProps> = ({ elevator }) => {
         </>
       )}
       {isAtDestination && (
-        <p>Framme, i våning {elevator.destination}</p>
+        <>
+          <span className={styles['elevator-direction']}>
+            <Image src={'/elevator.svg'} alt="Hissen är stillastående" width={200} height={50} priority />
+          </span>
+          <span className={styles['elevator-floor']}>
+            Framme, i våning {elevator.destination}
+          </span>
+        </>
       )}
-
       {error && <Error errorId="technicalError" errorMsg={`Tyvärr inträffade ett tekniskt fel "${error}"`} />}
     </div>
   );
